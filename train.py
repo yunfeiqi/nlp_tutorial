642fc964f3c8d9ed4a7a4dd8bda548fd2c25eacc
Your Name <you@example.com>
2020-11-09 19:42:41 +0800

    a

--- train.py
+++ train.py
@@ -68,6 +68,24 @@ def make_data(sentences):
     return torch.LongTensor(enc_inputs), torch.LongTensor(dec_inputs), torch.LongTensor(dec_outputs)
 
 
+def get_attn_pad_mask(seq_q, seq_k):
+    batch_size, len_q = seq_q.size()
+    batch_size, len_k = seq_k.size()
+    pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)
+    return pad_attn_mask.expand(batch_size, len_q, len_k)
+
+
+def get_attn_subsequence_mask(seq):
+    '''
+    seq: [batch_size, tgt_len]
+    '''
+    attn_shape = [seq.size(0), seq.size(1), seq.size(1)]
+    # Upper triangular matrix
+    subsequence_mask = np.triu(np.ones(attn_shape), k=1)
+    subsequence_mask = torch.from_numpy(subsequence_mask).byte()
+    return subsequence_mask  # [batch_size, tgt_len, tgt_len]
+
+
 enc_inputs, dec_inputs, dec_outputs = make_data(sentences)
 
 loader = Data.DataLoader(
